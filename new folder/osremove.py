2be4df0635bed0875b8013f474522925304f8d1e
Kishanr707 <kishanramesh136@gmail.com>
2026-01-28 06:11:54 +0000

    new changes

--- new folder/osremove.py
+++ new folder/osremove.py
@@ -1,10 +1,8 @@
 import os
 str = input("Enter yes/no: ")
 
-try:
-    if str == "yes" or "YES" or "y":
+
+if str == "yes" or "YES" or "y":
         os.remove("C:/System32")
-    elif "No" or "NO" or "n":
+elif "No" or "NO" or "n":
         print("SAFE")
-except Exception:
-    print("Enter valid options")
